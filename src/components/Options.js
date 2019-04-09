c88b623e832589909f6fe389d715e555669ba7a4
Rocky Green <rockjock51@gmail.com>
2019-04-08 23:15:22 -0500

    Day 9: Finishing up the first app

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,19 +4,25 @@ import Option from "./Option";
 
 const Options = (props) => (
     <div>
-        <button
-            onClick={props.handleDeleteOptions}
-            className="button button--link"
-        >
-            Remove All
-        </button>
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button
+                onClick={props.handleDeleteOptions}
+                className="button button--link"
+            >
+                Remove All
+            </button>
+        </div>
         {props.options.length === 0 && (
-            <p>Please add an option to get started!</p>
+            <p className="widget__message">
+                Please add an option to get started!
+            </p>
         )}
-        {props.options.map((option) => (
+        {props.options.map((option, index) => (
             <Option
                 optionText={option}
                 key={option}
+                count={index + 1}
                 handleDeleteOption={props.handleDeleteOption}
             />
         ))}
