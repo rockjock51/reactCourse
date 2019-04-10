c60e50c9b026544460cdae8233ab57ffb2754dbb
Rocky Green <rockjock51@gmail.com>
2019-04-09 21:56:38 -0500

    Day 10: Expensify App

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import ExpenseDashboard from "./components/ExpenseDashboard";
+import AddExpense from "./components/AddExpense";
+import EditExpense from "./components/EditExpense";
+import Help from "./components/help";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>this is my boilerplate</p>, document.getElementById("app"));
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboard} exact={true} />
+            <Route path="/create" component={AddExpense} />
+            <Route path="/edit" component={EditExpense} />
+            <Route path="/help" component={Help} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
---
+++ expensify-app/src/components/EditExpense.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <div>This is from the edit page</div>;
