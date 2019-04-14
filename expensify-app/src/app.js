4301f27731157b87f953cd9c0a733e5130b562c9
Rocky Green <rockjock51@gmail.com>
2019-04-14 18:08:24 -0500

    Day 15: Is this a redux course now?

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -5,5 +5,24 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense, removeExpense, editExpense } from "./actions/expenses";
+import {
+    setTextFilter,
+    setEndDate,
+    setStartDate,
+    sortByAmount,
+    sortByDate,
+} from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water Bill", amount: 10000 }));
+store.dispatch(addExpense({ description: "Gas Bill", amount: 20000 }));
+store.dispatch(setTextFilter("water"));
+console.log(store.getState());
+const state = store.getState();
+console.log(getVisibleExpenses(state.expenses, state.filters));
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
