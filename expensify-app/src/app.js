9174eb13aab939097862f724ac25572c1f04b956
Rocky Green <rockjock51@gmail.com>
2019-04-15 23:12:06 -0500

    Day 16: Connecting Store and Components

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -18,11 +19,21 @@ import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount: 10000 }));
+store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
 store.dispatch(addExpense({ description: "Gas Bill", amount: 20000 }));
 store.dispatch(setTextFilter("water"));
-console.log(store.getState());
+
+setTimeout(() => {
+    store.dispatch(setTextFilter("bill"));
+}, 3000);
+
 const state = store.getState();
 console.log(getVisibleExpenses(state.expenses, state.filters));
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
--- expensify-app/src/components/ExpenseDashboard.js
+++ expensify-app/src/components/ExpenseDashboard.js
@@ -1,3 +1,9 @@
 import React from "react";
 
-export default () => <div>This is from my dashboard component</div>;
+import ExpenseList from "./ExpenseList";
+
+export default () => (
+    <div>
+        <ExpenseList />
+    </div>
+);
