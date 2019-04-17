cc771a26394e77e09470a1a7120c6cb3809721eb
Rocky Green <rockjock51@gmail.com>
2019-04-16 23:31:10 -0500

    Day 17: Controlled Inputs for Filters

--- expensify-app/src/components/ExpenseDashboard.js
+++ expensify-app/src/components/ExpenseDashboard.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 import ExpenseList from "./ExpenseList";
+import ExpenseListFilters from "./ExpenseListFIlters";
 
 export default () => (
     <div>
+        <ExpenseListFilters />
         <ExpenseList />
     </div>
 );
