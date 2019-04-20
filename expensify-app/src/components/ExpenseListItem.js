c0ac95262344d2e99c45e29c87d3b7eae68c8a43
Rocky Green <rockjock51@gmail.com>
2019-04-19 22:25:10 -0500

    Day 20:  Editing expenses

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,22 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import { removeExpense } from "../actions/expenses";
 
 const ExpenseListItem = ({ description, amount, createdAt, id, dispatch }) => (
     <div>
-        <h3>{description}</h3>
+        <Link to={`/edit/${id}`}>{description}</Link>
         <p>
             {amount} - {createdAt}
         </p>
-        <button
-            onClick={(e) => {
-                dispatch(removeExpense({ id }));
-            }}
-        >
-            Remove
-        </button>
     </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
